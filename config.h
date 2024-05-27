33913b095395e454b8b82098be213a748705284a
CaryRock <carytherock@gmail.com>
2024-05-27 15:14:59 -0400

    It didn't actually take the change...?

--- config.h
+++ config.h
@@ -77,12 +77,12 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
-//static const char *termcmd[]  = { "st", NULL };
-//static const char scratchpadname[] = "scratchpad";
-//static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "alacritty", "-T", scratchpadname, "-o", "window.dimensions: {120,34}", NULL };
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
+//static const char *termcmd[]  = { "alacritty", NULL };
+//static const char scratchpadname[] = "scratchpad";
+//static const char *scratchpadcmd[] = { "alacritty", "-T", scratchpadname, "-o", "window.dimensions: {120,34}", NULL };
 
 #include <X11/XF86keysym.h>
 #include "shiftview.c"
