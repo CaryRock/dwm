1a975cfb48b3539abfb828bdf789a61fba7e92a9
CaryRock <carytherock@gmail.com>
2024-06-01 05:33:06 -0400

    Changed default main terminal to alacritty from wezterm. I don't think I was satisfied.

--- config.h
+++ config.h
@@ -78,11 +78,11 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 //static const char *termcmd[]  = { "st", NULL };
-static const char *termcmd[]  = { "wezterm", NULL };
+//static const char *termcmd[]  = { "wezterm", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 //static const char scratchpadname[] = "scratchpad";
-//static const char *scratchpadcmd[] = { "alacritty", "-T", scratchpadname, "-o", "window.dimensions: {120,34}", NULL };
 
 #include <X11/XF86keysym.h>
 #include "shiftview.c"
