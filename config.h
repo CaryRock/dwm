a8c27706213819cbca746e0895b5f4f859e950f8
CaryRock <carytherock@gmail.com>
2024-06-02 08:45:26 -0400

    Changed font sizes to 14, reverted back to using st for all terminals.

--- config.h
+++ config.h
@@ -12,7 +12,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 0;       /* vertical padding of bar */
 static const int sidepad            = 0;       /* horizontal padding of bar */
-static const char *fonts[]          = { "Noto Sans Mono:antialias=true:autohint=true:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
+static const char *fonts[]          = { "Noto Sans Mono:antialias=true:autohint=true:size=14", "JoyPixels:pixelsize=14:antialias=true:autohint=true"  };
 static char dmenufont[]       = "monospace:size=10";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
@@ -77,9 +77,9 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
-//static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st", NULL };
 //static const char *termcmd[]  = { "wezterm", NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+//static const char *termcmd[]  = { "alacritty", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 //static const char scratchpadname[] = "scratchpad";
